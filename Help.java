5607b32627878b2419c56a1c99a27eadd3057da0
iambestfy <37350082+EduardoDevelop3r@users.noreply.github.com>
2018-04-07 11:09:01 -0300

    Create Help.java

---
+++ Help.java
@@ -0,0 +1,22 @@
+package com.bestfyplugner.witherspawn.command;
+
+import org.bukkit.command.CommandSender;
+
+import com.bestfyplugner.api.manager.CommandManager;
+
+public class Help extends CommandManager {
+	 
+	public Help() {
+		super("help","ajuda");
+		// TODO Auto-generated constructor stub
+	}
+
+	public void command(CommandSender sender, String[] args) {
+		sender.sendMessage("§2-=-=-=-= §a§lWitherSpawn §2=-=-=-=-=");
+		sender.sendMessage("§b/wither setspawn §3Seta o Spawn do Wither");
+		sender.sendMessage("§b/wither spawn §3Spawna o Wither");
+	
+	
+	}
+
+}
